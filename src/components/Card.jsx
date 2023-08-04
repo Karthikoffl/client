2b2fc28dd530e710e30fe5fa518e5675f5c3452b
Karthikeyan N <109551096+Karthikoffl@users.noreply.github.com>
2023-08-04 16:13:34 +0530

    init

---
+++ src/components/Card.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import "../App.css"
+import { Link } from 'react-router-dom'
+
+export default function Card({post}) {
+  return (
+    <div className='card'>
+      <Link className='link' to={`/post/${post.id}`}>
+        <span className="title">{post.title}</span>
+        <img src={post.img} alt="" className="img" />
+        <p className="desc">{post.desc}</p>
+        <button className="cardButton">Read More</button>
+      </Link>
+    </div>
+  )
+}
